aff34ae249041c555b59c0ee8de33242cea881aa
martinbejarano <martinbejarano12@outlook.com>
2024-03-08 03:50:13 -0300

    styling

--- src/components/HourlyForecastBox.jsx
+++ src/components/HourlyForecastBox.jsx
@@ -4,7 +4,7 @@ export const HourlyForecastBox = ({ temp_c, condition, hour }) => {
     <div className="flex flex-col gap-2 p-2 border border-zinc-600/75 rounded-md items-center">
       <span>{hour}</span>
       <img src={condition} alt="condition" />
-      <span>{temp_c}</span>
+      <span>{temp_c}°</span>
     </div>
   );
 };
